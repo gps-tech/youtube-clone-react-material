2b372902ad7c47b2593a7382ae279eb475e21816
Gianmarco Pow-Sang <hello@gpstech.io>
2023-03-31 23:57:33 -0700

    working on fetching video data

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -6,10 +6,13 @@ import { SideBar, Videos } from "./";
 
 const Feed = () => {
   const [selectedCategory, setSelectedCategory] = useState("New");
+  const [videos, setVideos] = useState([]);
 
   useEffect(() => {
-    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`);
-  }, []);
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`).then((data) =>
+      setVideos(data.items)
+    );
+  }, [selectedCategory]);
 
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
@@ -41,6 +44,7 @@ const Feed = () => {
         >
           {selectedCategory} <span style={{ color: "#F31503" }}>videos</span>
         </Typography>
+        <Videos videos={videos} />
       </Box>
     </Stack>
   );
